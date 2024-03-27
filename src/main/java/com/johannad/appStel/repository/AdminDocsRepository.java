88f0a49206c0166ee8269542dac5a3ed30dcb8e0
stel_net <kjohannad@misena.edu.co>
2024-03-26 21:51:13 -0500

    Eliminar tabla docsadmin ya que no es relevante

--- src/main/java/com/johannad/appStel/repository/AdminDocsRepository.java
+++ src/main/java/com/johannad/appStel/repository/AdminDocsRepository.java
@@ -1,6 +1,5 @@
 package com.johannad.appStel.repository;
 
-import com.johannad.appStel.entity.AdminDocs;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
